52898af02366921d47a2b9b1cd9d653a1db39757
Hariom Modi <hariommodi7898@gmail.com>
2025-01-14 14:50:15 +0530

    Update aviator.js

--- aviator.js
+++ aviator.js
@@ -31,7 +31,7 @@ let k=0;
 // localStorage.setItem("num",1);
 refresh.addEventListener("click",()=>{
     // if(localStorage.getItem("num")==1){
-        localStorage.setItem("myutr",8989+k)
+        localStorage.setItem("myutr",143+k)
     // }
     // else{
         // myutr = 90;
