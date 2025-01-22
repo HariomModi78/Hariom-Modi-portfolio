5b6c6d2070f861777a1fb91b91fc990e601c6b1f
Hariom Modi <hariommodi7898@gmail.com>
2025-01-22 11:07:24 +0530

    Add files via upload

---
+++ todo.js
@@ -0,0 +1,82 @@
+let cover = document.querySelector(".cover");
+let button = document.querySelector("button");
+let input = document.querySelector("input");
+
+let box = [];
+let arr = [];
+let check = [];
+
+
+button.addEventListener("click",()=>{
+    if(input.value.trim() !== ""){
+localStorage.setItem("count",1)
+
+        let task = document.createElement("div");  // create a div
+        task.innerText = input.value;  // input ke value ka task
+        task.classList = "task";  // class of div is task
+        cover.appendChild(task);  // cover ke andar div dal dea 
+        let checkBox = document.createElement("input"); //input tag create kara 
+        checkBox.type = "checkbox";  //  input ka type checkbox dea
+        checkBox.classList = "checkBox";  // checkbox ko class checkBox de
+        task.appendChild(checkBox);  // checkbox ko task ke andar dala
+        input.value = "";   // fir input ke value ko remove kar dea
+        box.push(task.innerText);  // box array main div ko push kar dea
+        check.push(checkBox);
+        arr.push(task);
+        console.log(arr)
+        console.log(check)
+        console.log(box);
+         
+        localStorage.setItem("array",JSON.stringify(box));
+    }
+    for(let i=0;i<check.length;i++){
+        check[i].addEventListener("click",()=>{
+        arr[i].style.cssText = "    text-decoration:line-through";
+           
+            setTimeout(()=>{
+                let text = arr[i].innerText;
+            console.log(text)
+            cover.removeChild(arr[i]);
+            box[i].removeChild;
+            box = box.filter(item => item !== text);
+            localStorage.setItem("array",JSON.stringify(box));
+            },2000)
+        })
+    }
+
+})
+
+if(localStorage.getItem("count")!=null){
+    for(let i=0;i<JSON.parse(localStorage.getItem("array")).length;i++){
+        let task = document.createElement("div");
+        task.innerText =JSON.parse(localStorage.getItem("array"))[i];
+        task.classList = "task";
+        cover.appendChild(task);
+        let checkBox = document.createElement("input");
+        checkBox.type = "checkbox";
+        checkBox.classList = "checkBox";
+        task.appendChild(checkBox);
+        box.push(task.innerText);
+        check.push(checkBox);
+        arr.push(task);
+    }
+    
+}
+
+for(let i=0;i<check.length;i++){
+    check[i].addEventListener("click",()=>{
+        arr[i].style.cssText = "    text-decoration:line-through";
+
+        setTimeout(()=>{
+            let text = arr[i].innerText;
+        console.log(text)
+        cover.removeChild(arr[i]);
+        box[i].removeChild;
+        box = box.filter(item => item !== text);
+        localStorage.setItem("array",JSON.stringify(box));
+        },2000)
+    })
+}
+
+
+
