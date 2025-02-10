e3b2760f61c327c6d0e1ae6bc7ce23c579a3833f
Hariom Modi <hariommodi7898@gmail.com>
2025-02-10 22:34:57 +0530

    Update profile.js

--- profile.js
+++ profile.js
@@ -1,4 +1,8 @@
 let homeButton = document.querySelector(".ri-home-fill");
 homeButton.addEventListener("click",()=>{
     window.location.href = "main.html";
-})
+});
+let reel = document.querySelector(".ri-film-line");
+reel.addEventListener("click",()=>{
+    window.location.href = "reel.html";
+});
