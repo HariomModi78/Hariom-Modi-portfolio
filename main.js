bc39afc50eb998fd0d216732b09e3b71b3e1dbb3
Hariom Modi <hariommodi7898@gmail.com>
2025-02-10 22:42:35 +0530

    Update main.js

--- main.js
+++ main.js
@@ -5,4 +5,8 @@ profileButton.addEventListener("click",()=>{
 let login = document.querySelector(".story");
 login.addEventListener("click",()=>{
     window.location.href = "login.html";
+});
+let reel = document.querySelector(".ri-film-line");
+reel.addEventListener("click",()=>{
+    window.location.href = "reel.html";
 })
